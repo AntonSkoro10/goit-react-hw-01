0012537dd6f996f512f0820bd718179622da1121
AntonSkoro10 <antonskoro112@gmail.com>
2024-06-28 16:53:11 +0300

    updt profile

---
+++ src/components/App/App.jsx
@@ -0,0 +1,24 @@
+import userData from "../../userData.json"
+import friends from "../../friends.json"
+import transaction from "../../transaction.json"
+
+
+import Profile from "../Profile/Profile"
+import Friends from "../Friends/Friends"
+import TransactionHistory from "../TransactionHistory/TransactionHistory"
+
+export default function App() {
+    return (
+        <>
+            <Profile
+                name={userData.username}
+                tag={userData.tag}
+                location={userData.location}
+                image={userData.image}
+                stats={userData.stats}
+            />
+            <Friends friends={friends} />
+            <TransactionHistory item={transaction} />
+        </>
+    )
+}
